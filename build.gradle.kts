c4fe32277e5e6a1a6975455be8d2a8412c2f4a6e
xdsor <xdsor2100@gmail.com>
2024-01-04 00:22:23 +0600

    postgres support, replace tabs to four spaces

--- build.gradle.kts
+++ build.gradle.kts
@@ -26,9 +26,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.postgresql:postgresql")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
 	annotationProcessor("org.projectlombok:lombok")
+
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
 
--- src/test/java/ru/youmiteru/backend/BackendApplicationTests.java
+++ src/test/java/ru/youmiteru/backend/BackendApplicationTests.java
@@ -7,7 +7,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 class BackendApplicationTests {
 
 	@Test
-	void contextLoads() {
-	}
-
+    void contextLoads() {
+    }
 }
