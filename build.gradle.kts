c5edaebc72b5a24a7f286fbad0a9bc25936dbf5c
xdsor <xdsor2100@gmail.com>
2024-01-04 12:30:01 +0600

    feature #4: use h2 database in the unit tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -51,6 +51,7 @@ dependencies {
 	annotationProcessor("org.projectlombok:lombok")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("com.h2database:h2")
 }
 
 tasks.withType<Test> {
