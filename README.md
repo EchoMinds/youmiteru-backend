334c472bbc65769d66c8ffa326cb4a3d27ab7419
Andrii <andritokar@gmail.com>
2024-01-08 18:24:39 +0200

    made a Flyway migration file. Changer application.yml with new flyway properties

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-# youmiteru-backend test
+# youmiteru-backend
 Backend for the youmiteru website
 
+<<<<<<< HEAD
+=======
+[![codecov](https://codecov.io/gh/DJ-Java-Team/youmiteru-backend/graph/badge.svg?token=OI80Q5XKJD)](https://codecov.io/gh/DJ-Java-Team/youmiteru-backend)
+>>>>>>> dd734c19aee73f2ab105134037c4e2027e17cd17
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
 	java
+	jacoco
 	id("org.springframework.boot") version "3.2.1"
 	id("io.spring.dependency-management") version "1.1.4"
 }
@@ -9,6 +10,24 @@ version = "0.0.1-SNAPSHOT"
 
 java {
 	sourceCompatibility = JavaVersion.VERSION_17
+}
+
+jacoco {
+	toolVersion = "0.8.11"
+}
+
+tasks.withType<JacocoReport> {
+	reports {
+		xml.required.set(true)
+		html.required.set(true)
+	}
+
+	val mainSrc = sourceSets.getByName("main").output.classesDirs
+	classDirectories.setFrom(files(mainSrc).asFileTree.matching {
+		exclude("**/dto/**")
+		exclude("**/entity/**")
+		exclude("ru/youmiteru/backend/BackendApplication.class")
+	})
 }
 
 configurations {
@@ -26,12 +45,14 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
 	implementation("org.springframework.boot:spring-boot-starter-web")
+	implementation("org.flywaydb:flyway-core:10.4.1")
 	implementation("org.postgresql:postgresql")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("org.postgresql:postgresql")
+	runtimeOnly("org.flywaydb:flyway-database-postgresql:10.4.1")
 	annotationProcessor("org.projectlombok:lombok")
-
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("com.h2database:h2")
 }
 
 tasks.withType<Test> {
