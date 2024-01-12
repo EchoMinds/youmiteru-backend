f44138d30a4a75c7edceddf8b1d66286e591e2b7
Sjuice1 <76224197+Sjuice1@users.noreply.github.com>
2024-01-12 18:01:16 +0200

    Update V3__init_tables.sql

--- src/main/resources/db/migration/V3__init_tables.sql
+++ src/main/resources/db/migration/V3__init_tables.sql
@@ -32,7 +32,8 @@ create table if not exists youmiteru_backend.season(
     title_state varchar(64) not null,
     age_restriction varchar(24),
     year_season varchar(24),
-    anime_pictures jsonb
+    anime_pictures jsonb,
+    anime_format varchar(24)
 );
 
 create table if not exists youmiteru_backend.comment(
