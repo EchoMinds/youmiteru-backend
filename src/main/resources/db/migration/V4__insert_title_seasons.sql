a214064eefa223199ec53c5e1c419b21c07f0dbc
Tursonkul <telvintamer@gmail.com>
2024-01-12 21:07:01 +0600

    add V4 insert season, and V5 insert genres

---
+++ src/main/resources/db/migration/V4__insert_title_seasons.sql
@@ -0,0 +1,165 @@
+/*1 Тайтл*/
+INSERT INTO youmiteru_backend.title(title_image_url, name, description)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/55690/49d51b1c556632d0214a49831575b5e0.jpeg',
+        'Boku no Kokoro no Yabai Yatsu Season 2',
+        'Повседневная жизнь маленького социопата наполнена комичными ситуациями, которые можно увидеть, если смотреть аниме “Boku no Kokoro no Yabai Yatsu”. Очередная встреча Кётаро с очаровательной девчонкой произошла в книжном магазине, что выбило парня из колеи. Юноша не догадывается, что за милым образом скрывается совершенно другой человек.');
+/*Сезон1*/
+INSERT INTO youmiteru_backend.season(season_image_url, name, anime_format,
+                   description, release_date, title_id, title_state,
+                   age_restriction, year_season)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/52578/6ca77a2fc65011e3d7053304b8b001e3.jpeg',
+        'Boku no Kokoro no Yabai Yatsu Season 1', 'TV-show',
+        'Повседневная жизнь маленького социопата наполнена комичными ситуациями, которые можно увидеть, если смотреть аниме “Boku no Kokoro no Yabai Yatsu”. Очередная встреча Кётаро с очаровательной девчонкой произошла в книжном магазине, что выбило парня из колеи. Юноша не догадывается, что за милым образом скрывается совершенно другой человек.',
+        '2-04-2023', 1, 'FINISHED', '2023', '2023');
+/*Сезон2*/
+INSERT INTO youmiteru_backend.season(season_image_url, name, anime_format,
+                   description, release_date, title_id, title_state,
+                   age_restriction, year_season)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/55690/49d51b1c556632d0214a49831575b5e0.jpeg',
+        'Boku no Kokoro no Yabai Yatsu Season 2', 'TV-show',
+        'Повседневная жизнь маленького социопата наполнена комичными ситуациями, которые можно увидеть, если смотреть аниме “Boku no Kokoro no Yabai Yatsu”. Очередная встреча Кётаро с очаровательной девчонкой произошла в книжном магазине, что выбило парня из колеи. Юноша не догадывается, что за милым образом скрывается совершенно другой человек.',
+        '7-01-2024', 1, 'RELEASED', '2024', '2024');
+
+/*2 Тайтл*/
+INSERT INTO youmiteru_backend.title(title_image_url, name, description)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/35860/4650317350e47fa4f658d3c90ef656e1.jpeg',
+        'Karakai Jouzu no Takagi-san',
+        'В классе Нисикаты учится девочка по имени Такаги. Весёлая, симпатичная — глаз не оторвать. И — чудо! — сидит за соседней партой. Любой мальчишка на месте Нисикаты надул бы в штаны от счастья, однако для последнего Такаги — сущее наказание. Ведь именно его, бедного, бедного Нисикату она выбрала в качестве объекта для своих глупых розыгрышей.');
+
+/*Сезон1*/
+INSERT INTO youmiteru_backend.season(season_image_url, name, anime_format,
+                   description, release_date, title_id, title_state,
+                   age_restriction, year_season)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/35860/4650317350e47fa4f658d3c90ef656e1.jpeg',
+        'Karakai Jouzu no Takagi-san', 'TV-show',
+        'В классе Нисикаты учится девочка по имени Такаги. Весёлая, симпатичная — глаз не оторвать. И — чудо! — сидит за соседней партой. Любой мальчишка на месте Нисикаты надул бы в штаны от счастья, однако для последнего Такаги — сущее наказание. Ведь именно его, бедного, бедного Нисикату она выбрала в качестве объекта для своих глупых розыгрышей.',
+        '8-01-2018', 2, 'FINISHED', '2018', '2018');
+/*Сезон2*/
+INSERT INTO youmiteru_backend.season(season_image_url, name, anime_format,
+                   description, release_date, title_id, title_state,
+                   age_restriction, year_season)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/38993/534699774d8b121395967a91c573a66e.jpeg',
+        'Karakai Jouzu no Takagi-san 2', 'TV-show',
+        'В классе Нисикаты учится девочка по имени Такаги. Весёлая, симпатичная — глаз не оторвать. И — чудо! — сидит за соседней партой. Любой мальчишка на месте Нисикаты надул бы в штаны от счастья, однако для последнего Такаги — сущее наказание. Ведь именно его, бедного, бедного Нисикату она выбрала в качестве объекта для своих глупых розыгрышей.',
+        '7-06-2019', 2, 'FINISHED', '2019', '2019');
+
+/*3 Тайтл*/
+INSERT INTO youmiteru_backend.title(title_image_url, name, description)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/52211/3656ad1fb3786e5c5eba5806335c8599.jpeg',
+        'Mashle', 'Взмахни волшебной палочкой, произнеси заклинание — так делаются все дела в мире магии, от работы по дому до сражений за судьбу мира. Кажется, что всё должно быть легко и занимательно в этом мире, где каждый сам себе волшебник, способный наколдовать счастья и денег.');
+
+/*Сезон1*/
+INSERT INTO youmiteru_backend.season(season_image_url, name, anime_format,
+                                     description, release_date, title_id, title_state,
+                                     age_restriction, year_season)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/52211/3656ad1fb3786e5c5eba5806335c8599.jpeg',
+        'Mashle', 'TV-show', 'Взмахни волшебной палочкой, произнеси заклинание — так делаются все дела в мире магии, от работы по дому до сражений за судьбу мира. Кажется, что всё должно быть легко и занимательно в этом мире, где каждый сам себе волшебник, способный наколдовать счастья и денег.',
+        '8-04-2023', 3, 'FINISHED', '2023', '2023');
+
+/*Сезон2*/
+INSERT INTO youmiteru_backend.season(season_image_url, name, anime_format,
+                                     description, release_date, title_id, title_state,
+                                     age_restriction, year_season)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/55813/378c027d13b5c9e8f01f370cb23da5e4.jpeg',
+        'Mashle: Shinkakusha Kouho Senbatsu Shiken-hen', 'TV-show',
+        'Взмахни волшебной палочкой, произнеси заклинание — так делаются все дела в мире магии, от работы по дому до сражений за судьбу мира. Кажется, что всё должно быть легко и занимательно в этом мире, где каждый сам себе волшебник, способный наколдовать счастья и денег.',
+        '6-01-2024', 3, 'RELEASED', '2024', '2024');
+
+/*4 Тайтл*/
+INSERT INTO youmiteru_backend.title(title_image_url, name, description)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/54492/37693ebdb70f8b6edfb27b0361f04b1f.jpeg',
+        'Kusuriya no Hitorigoto',
+        'Уже полгода прошло с того момента, как 17-летнюю Маомао похитили и заставили трудиться в императорском дворце обычной служанкой. Работа тяжёлая, но девушка решила не сдаваться, не унывать и честно вкалывать, пока её не отпустят на покой. Планы изменились, когда до Маомао дошли вести о том, что детей императора одолел серьёзный недуг. ');
+
+/*Сезон1*/
+INSERT INTO youmiteru_backend.season(season_image_url, name, anime_format,
+                                     description, release_date, title_id, title_state,
+                                     age_restriction, year_season)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/54492/37693ebdb70f8b6edfb27b0361f04b1f.jpeg',
+        'Kusuriya no Hitorigoto', 'TV-show',
+        'Уже полгода прошло с того момента, как 17-летнюю Маомао похитили и заставили трудиться в императорском дворце обычной служанкой. Работа тяжёлая, но девушка решила не сдаваться, не унывать и честно вкалывать, пока её не отпустят на покой. Планы изменились, когда до Маомао дошли вести о том, что детей императора одолел серьёзный недуг.',
+        '22-10-2023', 4, 'RELEASED', '2023', '2023');
+
+/*5 Тайтл*/
+INSERT INTO youmiteru_backend.title(title_image_url, name, description)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/31964/d94d5bd52edd33ec458fd338a2f0517c.jpeg',
+        'Boku no Hero Academia',
+        'Четырнадцатилетний Идзуку Мидория рано осознал, что люди не рождаются равными. А пришло это понимание, когда его начали дразнить одноклассники, одарённые особой силой. Несмотря на то, что большинство людей в этом мире рождаются с необычными способностями, Идзуку оказался среди тех немногих, кто напрочь их лишён.');
+
+/*Сезон1*/
+INSERT INTO youmiteru_backend.season(season_image_url, name, anime_format,
+                                     description, release_date, title_id, title_state,
+                                     age_restriction, year_season)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/31964/d94d5bd52edd33ec458fd338a2f0517c.jpeg',
+        'Boku no Hero Academia', 'TV-show',
+        'Четырнадцатилетний Идзуку Мидория рано осознал, что люди не рождаются равными. А пришло это понимание, когда его начали дразнить одноклассники, одарённые особой силой. Несмотря на то, что большинство людей в этом мире рождаются с необычными способностями, Идзуку оказался среди тех немногих, кто напрочь их лишён.',
+        '1-12-2016', 5, 'FINISHED', '2016', '2016');
+
+/*Сезон2*/
+INSERT INTO youmiteru_backend.season(season_image_url, name, anime_format,
+                                     description, release_date, title_id, title_state,
+                                     age_restriction, year_season)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/33486/de1e7a755b37a416347ba4e0bb9de7d7.jpeg',
+        'Boku no Hero Academia 2nd Season', 'TV-show',
+        'Четырнадцатилетний Идзуку Мидория рано осознал, что люди не рождаются равными. А пришло это понимание, когда его начали дразнить одноклассники, одарённые особой силой. Несмотря на то, что большинство людей в этом мире рождаются с необычными способностями, Идзуку оказался среди тех немногих, кто напрочь их лишён.',
+        '17-04-2017', 5, 'FINISHED', '2017', '2017');
+
+/*Сезон3*/
+INSERT INTO youmiteru_backend.season(season_image_url, name, anime_format,
+                                     description, release_date, title_id, title_state,
+                                     age_restriction, year_season)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/36456/93bcf75afeed32394232db7803685f38.jpeg',
+        'Boku no Hero Academia 3rd Season', 'TV-show',
+        'Четырнадцатилетний Идзуку Мидория рано осознал, что люди не рождаются равными. А пришло это понимание, когда его начали дразнить одноклассники, одарённые особой силой. Несмотря на то, что большинство людей в этом мире рождаются с необычными способностями, Идзуку оказался среди тех немногих, кто напрочь их лишён.',
+        '7-04-2018', 5, 'FINISHED', '2018', '2018');
+
+/*Сезон4*/
+INSERT INTO youmiteru_backend.season(season_image_url, name, anime_format,
+                                     description, release_date, title_id, title_state,
+                                     age_restriction, year_season)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/38408/c688f6f111eddb56760e3580e4ed3abe.jpeg',
+        'Boku no Hero Academia 4th Season', 'TV-show',
+        'Четырнадцатилетний Идзуку Мидория рано осознал, что люди не рождаются равными. А пришло это понимание, когда его начали дразнить одноклассники, одарённые особой силой. Несмотря на то, что большинство людей в этом мире рождаются с необычными способностями, Идзуку оказался среди тех немногих, кто напрочь их лишён.',
+        '7-04-2019', 5, 'FINISHED', '2019', '2019');
+
+/*Сезон5*/
+INSERT INTO youmiteru_backend.season(season_image_url, name, anime_format,
+                                     description, release_date, title_id, title_state,
+                                     age_restriction, year_season)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/41587/3c927845258dcf5bbb81b1b03795f1d4.jpeg',
+        'Boku no Hero Academia 5th Season', 'TV-show',
+        'Четырнадцатилетний Идзуку Мидория рано осознал, что люди не рождаются равными. А пришло это понимание, когда его начали дразнить одноклассники, одарённые особой силой. Несмотря на то, что большинство людей в этом мире рождаются с необычными способностями, Идзуку оказался среди тех немногих, кто напрочь их лишён.',
+        '27-03-2021', 5, 'FINISHED', '2021', '2021');
+
+/*Сезон6*/
+INSERT INTO youmiteru_backend.season(season_image_url, name, anime_format,
+                                     description, release_date, title_id, title_state,
+                                     age_restriction, year_season)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/49918/6aeb68676ce8f628ec1e923284de5f27.jpeg',
+        'Boku no Hero Academia 6th Season', 'TV-show',
+        'Четырнадцатилетний Идзуку Мидория рано осознал, что люди не рождаются равными. А пришло это понимание, когда его начали дразнить одноклассники, одарённые особой силой. Несмотря на то, что большинство людей в этом мире рождаются с необычными способностями, Идзуку оказался среди тех немногих, кто напрочь их лишён.',
+        '27-03-2022', 5, 'FINISHED', '2022', '2022');
+
+/*Сезон7*/
+INSERT INTO youmiteru_backend.season(season_image_url, name, anime_format,
+                                     description, release_date, title_id, title_state,
+                                     age_restriction, year_season)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/54789/224f14f20b5aa2074d30aaa7cce727fa.jpeg',
+        'Boku no Hero Academia 7th Season', 'TV-show',
+        'Четырнадцатилетний Идзуку Мидория рано осознал, что люди не рождаются равными. А пришло это понимание, когда его начали дразнить одноклассники, одарённые особой силой. Несмотря на то, что большинство людей в этом мире рождаются с необычными способностями, Идзуку оказался среди тех немногих, кто напрочь их лишён.',
+        '4-05-2024', 5, 'RELEASED', '2024', '2024');
+
+/*6 Тайтл*/
+INSERT INTO youmiteru_backend.title(title_image_url, name, description)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/52741/b0ef01c548d1b54bee1439a7e84fb364.jpeg',
+        'Undead Unluck',
+        'Человек — существо социальное, ему требуется взаимодействие с окружающими не только посредством диалога, но и тактильного контакта. Увы, но Фуко Идзумо лишена этого удовольствия из-за своей «особенности» приносить неудачу через прикосновение.');
+
+/*Сезон1*/
+INSERT INTO youmiteru_backend.season(season_image_url, name, anime_format,
+                                     description, release_date, title_id, title_state,
+                                     age_restriction, year_season)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/52741/b0ef01c548d1b54bee1439a7e84fb364.jpeg',
+        'Undead Unluck', 'TV-show',
+        'Человек — существо социальное, ему требуется взаимодействие с окружающими не только посредством диалога, но и тактильного контакта. Увы, но Фуко Идзумо лишена этого удовольствия из-за своей «особенности» приносить неудачу через прикосновение.',
+        '7-10-2023', 6, 'RELEASED', '2023', '2023');
