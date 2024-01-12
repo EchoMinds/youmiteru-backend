c06e14d4e6f693ed9d136127f50948af7129337f
Tursonkul <telvintamer@gmail.com>
2024-01-12 15:37:08 +0600

    add insert title, seasons, genres

---
+++ src/main/resources/db/migration/insert_title_seasons.sql
@@ -0,0 +1,44 @@
+/*1 Тайтл*/
+INSERT INTO title(title_image_url, name, description)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/55690/49d51b1c556632d0214a49831575b5e0.jpeg',
+        'Boku no Kokoro no Yabai Yatsu Season 2',
+        'Повседневная жизнь маленького социопата наполнена комичными ситуациями, которые можно увидеть, если смотреть аниме “Boku no Kokoro no Yabai Yatsu”. Очередная встреча Кётаро с очаровательной девчонкой произошла в книжном магазине, что выбило парня из колеи. Юноша не догадывается, что за милым образом скрывается совершенно другой человек.');
+/*Сезон1*/
+INSERT INTO season(season_image_url, name, anime_format,
+                   description, release_date, title_id, title_state,
+                   age_restriction, year_season)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/52578/6ca77a2fc65011e3d7053304b8b001e3.jpeg',
+        'Boku no Kokoro no Yabai Yatsu Season 1', 'TV-show',
+        'Повседневная жизнь маленького социопата наполнена комичными ситуациями, которые можно увидеть, если смотреть аниме “Boku no Kokoro no Yabai Yatsu”. Очередная встреча Кётаро с очаровательной девчонкой произошла в книжном магазине, что выбило парня из колеи. Юноша не догадывается, что за милым образом скрывается совершенно другой человек.',
+        '2-04-2023', 1, 'FINISHED', '2023', '2023');
+/*Сезон2*/
+INSERT INTO season(season_image_url, name, anime_format,
+                   description, release_date, title_id, title_state,
+                   age_restriction, year_season)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/55690/49d51b1c556632d0214a49831575b5e0.jpeg',
+        'Boku no Kokoro no Yabai Yatsu Season 2', 'TV-show',
+        'Повседневная жизнь маленького социопата наполнена комичными ситуациями, которые можно увидеть, если смотреть аниме “Boku no Kokoro no Yabai Yatsu”. Очередная встреча Кётаро с очаровательной девчонкой произошла в книжном магазине, что выбило парня из колеи. Юноша не догадывается, что за милым образом скрывается совершенно другой человек.',
+        '7-01-2024', 1, 'RELEASED', '2024', '2024');
+
+/*2 Тайтл*/
+INSERT INTO title(title_image_url, name, description)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/35860/4650317350e47fa4f658d3c90ef656e1.jpeg',
+        'Karakai Jouzu no Takagi-san',
+        'В классе Нисикаты учится девочка по имени Такаги. Весёлая, симпатичная — глаз не оторвать. И — чудо! — сидит за соседней партой. Любой мальчишка на месте Нисикаты надул бы в штаны от счастья, однако для последнего Такаги — сущее наказание. Ведь именно его, бедного, бедного Нисикату она выбрала в качестве объекта для своих глупых розыгрышей.');
+
+/*Сезон1*/
+INSERT INTO season(season_image_url, name, anime_format,
+                   description, release_date, title_id, title_state,
+                   age_restriction, year_season)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/35860/4650317350e47fa4f658d3c90ef656e1.jpeg',
+        'Karakai Jouzu no Takagi-san', 'TV-show',
+        'В классе Нисикаты учится девочка по имени Такаги. Весёлая, симпатичная — глаз не оторвать. И — чудо! — сидит за соседней партой. Любой мальчишка на месте Нисикаты надул бы в штаны от счастья, однако для последнего Такаги — сущее наказание. Ведь именно его, бедного, бедного Нисикату она выбрала в качестве объекта для своих глупых розыгрышей.',
+        '8-01-2018', 2, 'FINISHED', '2018', '2018');
+/*Сезон2*/
+INSERT INTO season(season_image_url, name, anime_format,
+                   description, release_date, title_id, title_state,
+                   age_restriction, year_season)
+VALUES ('https://desu.shikimori.one/uploads/poster/animes/38993/534699774d8b121395967a91c573a66e.jpeg',
+        'Karakai Jouzu no Takagi-san 2', 'TV-show',
+        'В классе Нисикаты учится девочка по имени Такаги. Весёлая, симпатичная — глаз не оторвать. И — чудо! — сидит за соседней партой. Любой мальчишка на месте Нисикаты надул бы в штаны от счастья, однако для последнего Такаги — сущее наказание. Ведь именно его, бедного, бедного Нисикату она выбрала в качестве объекта для своих глупых розыгрышей.',
+        '7-06-2019', 2, 'FINISHED', '2019', '2019');
