a1551c1d8dd2b1ae684c40be80eedb32ec7532b1
RainGor <kota-2019@inbox.ru>
2024-02-01 21:40:09 +0300

    Добавил все че было в модели в дто как интерфейс

--- src/main/java/ru/youmiteru/backend/dto/SeasonDTO.java
+++ src/main/java/ru/youmiteru/backend/dto/SeasonDTO.java
@@ -4,25 +4,14 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import ru.youmiteru.backend.domain.TitleState;
+import java.time.LocalDate;
 import java.util.List;
 
 public class SeasonDTO {
-
-
     private interface seasonId {
         @JsonProperty(value = "seasons_id")
         Long getSeasonId();
-    }
-
-    private interface seasonName {
-        @JsonProperty(value = "seasons_name")
-        String getSeasonName();
-    }
-
-    private interface description {
-        @JsonProperty(value = "seasons_description")
-        String getDescription();
     }
 
     private interface imageUrl {
@@ -30,11 +19,52 @@ public class SeasonDTO {
         String getImageUrl();
     }
 
+    private interface seasonName {
+        @JsonProperty(value = "seasons_name")
+        String getSeasonName();
+    }
+
+    private interface animeFormat {
+        @JsonProperty(value = "anime_format")
+        String getAnimeFormat();
+    }
+
+    private interface description {
+        @JsonProperty(value = "seasons_description")
+        String getDescription();
+    }
+
+    private interface releaseDate {
+        @JsonProperty(value = "release_date")
+        LocalDate getReleaseDate();
+    }
+
+    private interface titleId {
+        @JsonProperty(value = "title_id")
+        Long getTitleId();
+    }
+
+    private interface titleState {
+        @JsonProperty(value = "title_id")
+        TitleState getTitleState();
+    }
+
+    private interface ageRestriction {
+        @JsonProperty(value = "age_restriction")
+        String getAgeRestriction();
+    }
+
+    private interface yearSeason {
+        @JsonProperty(value = "year_season")
+        String getYearSeason();
+    }
+
+
     public enum Response {;
         @NoArgsConstructor
         @AllArgsConstructor
         @Data
-        public static class HomePage implements seasonId, seasonName, description, imageUrl{
+        public static class HomePage implements seasonId, seasonName, description, imageUrl {
             Long seasonId;
             String seasonName;
             String description;
