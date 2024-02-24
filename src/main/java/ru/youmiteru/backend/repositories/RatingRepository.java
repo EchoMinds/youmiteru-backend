247faad491866be7369f1036960268b5d4f34aa0
RainGor <kota-2019@inbox.ru>
2024-02-24 20:09:22 +0300

    * RatingRepository

--- src/main/java/ru/youmiteru/backend/repositories/RatingRepository.java
+++ src/main/java/ru/youmiteru/backend/repositories/RatingRepository.java
@@ -4,6 +4,8 @@ import org.hibernate.mapping.Value;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.youmiteru.backend.domain.Rating;
 
@@ -12,5 +14,6 @@ import java.util.stream.Collectors;
 
 @Repository
 public interface RatingRepository extends JpaRepository<Rating,Long> {
-    List<Rating> findAllBySeason_Id(Long id);
+    @Query(value = "select ROUND(AVG(value),1) from youmiteru_backend.rating where season_id = :seasonId " ,nativeQuery = true)
+    Double getRatingBySeasonId(@Param("seasonId") Long seasonId);
 }
--- src/main/java/ru/youmiteru/backend/service/RatingService.java
+++ src/main/java/ru/youmiteru/backend/service/RatingService.java
@@ -12,20 +12,7 @@ import java.util.List;
 public class RatingService {
     private final RatingRepository ratingRepository;
 
-
-    //get rating
     public Double getRating(Long id) {
-
-        //get class rating for season
-        List<Rating> valueListWithRaitingClass = ratingRepository.findAllBySeason_Id(id);
-        //get only value
-        List<Integer> ratings = valueListWithRaitingClass.stream().map(Rating::getValue).toList();
-
-        double rating = ratings.stream().mapToDouble(d -> d).average().orElse(0.0);
-        double scaleRounding = 100;
-        //rounding
-        double result = Math.ceil(rating * scaleRounding) / scaleRounding;
-
-        return result;
+        return ratingRepository.getRatingBySeasonId(id);
     }
 }
