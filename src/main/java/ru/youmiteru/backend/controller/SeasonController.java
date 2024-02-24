c117f62085a0d4c37983ec74390f84183686ba4c
RainGor <kota-2019@inbox.ru>
2024-02-24 20:24:54 +0300

    * hotfix

--- src/main/java/ru/youmiteru/backend/controller/SeasonController.java
+++ src/main/java/ru/youmiteru/backend/controller/SeasonController.java
@@ -12,7 +12,7 @@ import ru.youmiteru.backend.dto.SeasonDTO;
 import ru.youmiteru.backend.service.SeasonService;
 
 @RestController
-@RequestMapping("/youmiteru")
+@RequestMapping("/api/v1")
 @RequiredArgsConstructor
 public class SeasonController {
     private final SeasonService seasonService;
