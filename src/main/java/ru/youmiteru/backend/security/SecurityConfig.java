cbc747e2abb53132eed06fdc67d2312ab6ca7e79
Tursonkul <telvintamer@gmail.com>
2024-03-24 20:16:45 +0500

    поменял названия пакета на security и все)

--- src/main/java/ru/youmiteru/backend/security/SecurityConfig.java
+++ src/main/java/ru/youmiteru/backend/security/SecurityConfig.java
@@ -1,4 +1,4 @@
-package ru.youmiteru.backend.config;
+package ru.youmiteru.backend.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
